9c21bdb0938d7a8f620a9ceae1c6f5564508ca60
MBOBDA AYMAR IVAN <aymarmbobda1597@gmail.com>
2020-07-30 02:46:54 +0100

    Correction of repositories

--- src/main/java/com/maitech/repositories/RoleRepository.java
+++ src/main/java/com/maitech/repositories/RoleRepository.java
@@ -2,7 +2,9 @@ package com.maitech.repositories;
 
 import com.maitech.models.RoleModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends MongoRepository<RoleModel, String> {
     public RoleModel findByName(String name);
     public RoleModel findByCode(String code);
--- src/main/java/com/maitech/repositories/UserRepository.java
+++ src/main/java/com/maitech/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.maitech.repositories;
 import com.maitech.models.RoleModel;
 import com.maitech.models.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
     public UserModel findByPhone(int phone);
     public UserModel findByEmail(String email);

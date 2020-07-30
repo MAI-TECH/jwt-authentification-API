1a076056f4956a582f3143abe79b98f7fbedeb03
MBOBDA AYMAR IVAN <aymarmbobda1597@gmail.com>
2020-07-30 05:09:02 +0100

    Correction of saveUser fonction

--- src/main/java/com/maitech/services/UserService.java
+++ src/main/java/com/maitech/services/UserService.java
@@ -3,7 +3,9 @@ package com.maitech.services;
 import com.maitech.models.UserModel;
 import com.maitech.repositories.RoleRepository;
 import com.maitech.repositories.UserRepository;
+import com.maitech.utils.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +24,12 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private Validation validation;
+
     public List<UserModel> findAllUsers() {
         return userRepository.findAll();
     }
@@ -29,39 +37,45 @@ public class UserService {
     public UserModel findUserById(String idUserModel) {
         UserModel userModel = userRepository.findById(idUserModel).get();
 
-        if (userModel != null) {
-            return userModel;
-        }
-        else {
-            return null;
-        }
+        return (userModel != null) ? userModel : null;
+    }
+
+    public UserModel findUserByPhone(int phone) {
+        UserModel userModel = userRepository.findByPhone(phone);
+
+        return (userModel != null) ? userModel : null;
+    }
+
+    public UserModel findUserByEmail(String email) {
+        UserModel userModel = userRepository.findByEmail(email);
+
+        return (userModel != null) ? userModel : null;
     }
 
     public UserModel findUserByUsername(String username) {
         UserModel userModel = userRepository.findByUsername(username);
 
-        if (userModel != null) {
-            return userModel;
-        }
-        else {
-            return null;
-        }
+        return (userModel != null) ? userModel : null;
     }
 
-    public UserModel saveUser(UserModel userModel) throws Exception {
+    public UserModel saveUser(@org.jetbrains.annotations.NotNull UserModel userModel) throws Exception {
         UserModel user = new UserModel();
         List<String> errorList = new ArrayList<>();
 
         if (userModel.getIdUserModel() != null)
             user.setIdUserModel(userModel.getIdUserModel());
 
+        if (userModel.getLastName() == null || userModel.getLastName().length() < 5)
+            errorList.add("The lastName of user could not empty");
         user.setLastName(userModel.getFirstName());
+
+        if (userModel.getFirstName() == null || userModel.getFirstName().length() < 3)
+            errorList.add("The firstName of user could not empty");
         user.setFirstName(userModel.getFirstName());
-        /*
+
         if (!userModel.getGender().toUpperCase().equals("HOMME") && !userModel.getGender().toUpperCase().equals("FEMME"))
             errorList.add("Unexpected value : " + user.getGender());
         user.setGender(userModel.getGender().toUpperCase());
-        user.setTrade(userModel.getTrade());
 
         if (!validation.isValidPhone(""+ userModel.getPhone() + ""))
             errorList.add("ERROR : Phone is not valid");
@@ -70,15 +84,15 @@ public class UserService {
         if (!validation.isValidEmail(userModel.getEmail()))
             errorList.add("Error : Email is not valid");
         user.setEmail(userModel.getEmail());
-        */
+
         if (userModel.getUsername().isEmpty())
             errorList.add("ERROR : Username is not valid");
         user.setUsername(userModel.getUsername());
-        /*
+
         if (!validation.isValidPassword(userModel.getPassword()))
             errorList.add("Error");
         user.setPassword(bCryptPasswordEncoder.encode(userModel.getPassword()));
-        */
+
         if (roleRepository.findByName(userModel.getRoleModel().getName()) == null)
             errorList.add("Unexpected Value : " + userModel.getRoleModel().getName());
         user.setRoleModel(roleRepository.findByName(userModel.getRoleModel().getName()));
@@ -96,6 +110,30 @@ public class UserService {
 
     public boolean deleteUserById(String idUserModel) {
         UserModel userModel = userRepository.findById(idUserModel).get();
+
+        if (userModel != null) {
+            userRepository.delete(userModel);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean deleteUserByPhone(int phone) {
+        UserModel userModel = userRepository.findByPhone(phone);
+
+        if (userModel != null) {
+            userRepository.delete(userModel);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean deleteUserByEmail(String email) {
+        UserModel userModel = userRepository.findByEmail(email);
 
         if (userModel != null) {
             userRepository.delete(userModel);
